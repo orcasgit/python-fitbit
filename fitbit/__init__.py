eb2a5126befc720bf8d02e74e1c88c848f5498cc
Brad Pitcher <bradpitcher@gmail.com>
2014-09-18 16:37:56 -0700

    v0.1.1

--- fitbit/__init__.py
+++ fitbit/__init__.py
@@ -17,7 +17,7 @@ __author_email__ = 'bpitcher@orcasinc.com'
 __copyright__ = 'Copyright 2012-2014 ORCAS'
 __license__ = 'Apache 2.0'
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # Module namespace.
 
