a655f4825d1c2c44318968ccc7e6d02218681ebc
reyvababtista <reyvababtista16@gmail.com>
2022-03-25 16:02:15 -0500

    refactor(repository): disable csv

--- repository/fitbit_repository.py
+++ repository/fitbit_repository.py
@@ -26,7 +26,7 @@ class Repository():
         print("--------------------------------------------------\n")
         self.rdb.store_profile(profile)
         self.firestore.store_profile(profile)
-        self.csv.store_profile(profile)
+        # self.csv.store_profile(profile)
         return profile['encodedId']
 
     def get_intraday(self):
@@ -34,25 +34,25 @@ class Repository():
             resource="steps")
         self.rdb.store_intraday(data=steps, date=self.date, doc_name="steps")
         self.firestore.store_intraday(data=steps, date=self.date, doc_name="steps")
-        self.csv.store_intraday(data=steps, date=self.date, doc_name="steps")
+        # self.csv.store_intraday(data=steps, date=self.date, doc_name="steps")
 
         calories = self.fitbit.intraday_time_series(
             resource="calories")
         self.rdb.store_intraday(data=calories, date=self.date, doc_name="calories")
         self.firestore.store_intraday(data=calories, date=self.date, doc_name="calories")
-        self.csv.store_intraday(data=calories, date=self.date, doc_name="calories")
+        # self.csv.store_intraday(data=calories, date=self.date, doc_name="calories")
         
         distance = self.fitbit.intraday_time_series(
             resource="distance")
         self.rdb.store_intraday(data=distance, date=self.date, doc_name="distance")
         self.firestore.store_intraday(data=distance, date=self.date, doc_name="distance")
-        self.csv.store_intraday(data=calories, date=self.date, doc_name="calories")
+        # self.csv.store_intraday(data=calories, date=self.date, doc_name="calories")
 
         heart = self.fitbit.intraday_time_series(
             resource="heart")
         self.rdb.store_intraday(data=heart, date=self.date, doc_name="heart")
         self.firestore.store_intraday(data=heart, date=self.date, doc_name="heart")
-        self.csv.store_intraday(data=heart, date=self.date, doc_name="heart")
+        # self.csv.store_intraday(data=heart, date=self.date, doc_name="heart")
 
         try:
             elevation = self.fitbit.intraday_time_series(
@@ -61,7 +61,7 @@ class Repository():
                 end_date=self.date)
             self.rdb.store_intraday(data=elevation, date=self.date, doc_name="elevation")
             self.firestore.store_intraday(data=elevation, date=self.date, doc_name="elevation")
-            self.csv.store_intraday(data=elevation, date=self.date, doc_name="elevation")
+            # self.csv.store_intraday(data=elevation, date=self.date, doc_name="elevation")
         except HTTPBadRequest as e:
             self._log_err(e)
 
@@ -72,7 +72,7 @@ class Repository():
                 end_date=self.date)
             self.rdb.store_intraday(data=floors, date=self.date, doc_name="floors")
             self.firestore.store_intraday(data=floors, date=self.date, doc_name="floors")
-            self.csv.store_intraday(data=floors, date=self.date, doc_name="floors")
+            # self.csv.store_intraday(data=floors, date=self.date, doc_name="floors")
         except HTTPBadRequest as e:
             self._log_err(e)
     
@@ -84,7 +84,7 @@ class Repository():
             api_version=1.2)
         self.rdb.store_time_series(data=sleeps, doc_name="sleeps")
         self.firestore.store_time_series(data=sleeps, doc_name="sleeps")
-        self.csv.store_time_series(data=sleeps, doc_name="sleeps")
+        # self.csv.store_time_series(data=sleeps, doc_name="sleeps")
 
         heart_rates = self.fitbit.time_series(
             resource='activities/heart',
@@ -92,7 +92,7 @@ class Repository():
             end_date=self.date,)
         self.rdb.store_time_series(data=heart_rates, doc_name="heart_rates")
         self.firestore.store_time_series(data=heart_rates, doc_name="heart_rates")
-        self.csv.store_time_series(data=heart_rates, doc_name="heart_rates")
+        # self.csv.store_time_series(data=heart_rates, doc_name="heart_rates")
 
         activity_calories = self.fitbit.time_series(
             resource='activities/activityCalories',
@@ -100,7 +100,7 @@ class Repository():
             end_date=self.date,)
         self.rdb.store_time_series(data=activity_calories, doc_name="activity_calories")
         self.firestore.store_time_series(data=activity_calories, doc_name="activity_calories")
-        self.csv.store_time_series(data=activity_calories, doc_name="activity_calories")
+        # self.csv.store_time_series(data=activity_calories, doc_name="activity_calories")
 
         calories = self.fitbit.time_series(
             resource='activities/calories',
@@ -108,7 +108,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=calories, doc_name="calories")
         self.firestore.store_time_series(data=calories, doc_name="calories")
-        self.csv.store_time_series(data=calories, doc_name="calories")
+        # self.csv.store_time_series(data=calories, doc_name="calories")
 
         calories_bmr = self.fitbit.time_series(
             resource='activities/caloriesBMR',
@@ -116,7 +116,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=calories_bmr, doc_name="calories_bmr")
         self.firestore.store_time_series(data=calories_bmr, doc_name="calories_bmr")
-        self.csv.store_time_series(data=calories_bmr, doc_name="calories_bmr")
+        # self.csv.store_time_series(data=calories_bmr, doc_name="calories_bmr")
 
         distance = self.fitbit.time_series(
             resource='activities/distance',
@@ -124,7 +124,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=distance, doc_name="distance")
         self.firestore.store_time_series(data=distance, doc_name="distance")
-        self.csv.store_time_series(data=distance, doc_name="distance")
+        # self.csv.store_time_series(data=distance, doc_name="distance")
 
         try:
             elevation = self.fitbit.time_series(
@@ -133,7 +133,7 @@ class Repository():
                 end_date=self.date)
             self.rdb.store_time_series(data=elevation, doc_name="elevation")
             self.firestore.store_time_series(data=elevation, doc_name="elevation")
-            self.csv.store_time_series(data=elevation, doc_name="elevation")
+            # self.csv.store_time_series(data=elevation, doc_name="elevation")
         except HTTPBadRequest as e:
             self._log_err(e)
 
@@ -144,7 +144,7 @@ class Repository():
                 end_date=self.date)
             self.rdb.store_time_series(data=floors, doc_name="floors")
             self.firestore.store_time_series(data=floors, doc_name="floors")
-            self.csv.store_time_series(data=floors, doc_name="floors")
+            # self.csv.store_time_series(data=floors, doc_name="floors")
         except HTTPBadRequest as e:
             self._log_err(e)
 
@@ -154,7 +154,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=minutes_sedentary, doc_name="minutes_sedentary")
         self.firestore.store_time_series(data=minutes_sedentary, doc_name="minutes_sedentary")
-        self.csv.store_time_series(data=minutes_sedentary, doc_name="minutes_sedentary")
+        # self.csv.store_time_series(data=minutes_sedentary, doc_name="minutes_sedentary")
 
         minutes_lightly_active = self.fitbit.time_series(
             resource='activities/minutesLightlyActive',
@@ -162,7 +162,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=minutes_lightly_active, doc_name="minutes_lightly_active")
         self.firestore.store_time_series(data=minutes_lightly_active, doc_name="minutes_lightly_active")
-        self.csv.store_time_series(data=minutes_lightly_active, doc_name="minutes_lightly_active")
+        # self.csv.store_time_series(data=minutes_lightly_active, doc_name="minutes_lightly_active")
 
         minutes_fairly_active = self.fitbit.time_series(
             resource='activities/minutesFairlyActive',
@@ -170,7 +170,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=minutes_fairly_active, doc_name="minutes_fairly_active")
         self.firestore.store_time_series(data=minutes_fairly_active, doc_name="minutes_fairly_active")
-        self.csv.store_time_series(data=minutes_fairly_active, doc_name="minutes_fairly_active")
+        # self.csv.store_time_series(data=minutes_fairly_active, doc_name="minutes_fairly_active")
 
         minutes_very_active = self.fitbit.time_series(
             resource='activities/minutesVeryActive',
@@ -178,7 +178,7 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=minutes_very_active, doc_name="minutes_very_active")
         self.firestore.store_time_series(data=minutes_very_active, doc_name="minutes_very_active")
-        self.csv.store_time_series(data=minutes_very_active, doc_name="minutes_very_active")
+        # self.csv.store_time_series(data=minutes_very_active, doc_name="minutes_very_active")
 
         steps = self.fitbit.time_series(
             resource='activities/steps',
@@ -186,4 +186,4 @@ class Repository():
             end_date=self.date)
         self.rdb.store_time_series(data=steps, doc_name="steps")
         self.firestore.store_time_series(data=steps, doc_name="steps")
-        self.csv.store_time_series(data=steps, doc_name="steps")
+        # self.csv.store_time_series(data=steps, doc_name="steps")
